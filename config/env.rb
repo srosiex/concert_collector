100b27cb40af46a17db10a2fc9782c10c4434355
Sarah Henderson <sarahrose.henderson@gmail.com>
2020-06-12 12:29:29 -0500

    updating for heroku

--- config/env.rb
+++ config/env.rb
@@ -12,3 +12,16 @@ end
 
 # ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 require_all 'app'
+
+configure :production do
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+ 
+    ActiveRecord::Base.establish_connection(
+      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+      )
+ end
